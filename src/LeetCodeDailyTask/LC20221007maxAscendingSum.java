542554c50f88964e96de2a5439087b68c7b0dfc3
Rookie-xixi <63187798+Rookie-xixi@users.noreply.github.com>
2022-10-07 12:18:13 +0800

    LCDT20221007

---
+++ src/LeetCodeDailyTask/LC20221007maxAscendingSum.java
@@ -0,0 +1,22 @@
+package LeetCodeDailyTask;
+
+public class LC20221007maxAscendingSum {
+    public static int maxAscendingSum(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int j = i, sum = nums[j];
+            while (j + 1 < n && nums[j + 1] > nums[j]) {
+                sum += nums[++j];
+            }
+            max = Math.max(max, sum);
+            i = j;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10,20,30,40,50};
+        System.out.println(maxAscendingSum(nums));
+    }
+}
