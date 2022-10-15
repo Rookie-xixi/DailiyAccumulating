c5f34d325f409ae019e1f6901390b96f3b380f2e
Rookie-xixi <63187798+Rookie-xixi@users.noreply.github.com>
2022-10-15 19:07:59 +0800

    314thWeekContest

--- src/LeetCodeWeeklyContest/WeeklyContset314_20221009/Main3.java
+++ src/LeetCodeWeeklyContest/WeeklyContset314_20221009/Main3.java
@@ -1,5 +1,37 @@
 package LeetCodeWeeklyContest.WeeklyContset314_20221009;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Main3 {
     //未做出
+    // 20221015看题解
+    // 灵神
+    // https://leetcode.cn/problems/using-a-robot-to-print-the-lexicographically-smallest-string/solution/tan-xin-zhan-by-endlesscheng-ldds/
+    public static String robotWithString(String s) {
+        char[] str = s.toCharArray();
+        int[] cnt = new int[26];
+        for (char c : str) {
+            cnt[c - 'a']++;
+        }
+        Deque<Character> stack = new ArrayDeque<>();
+        int min = 0;
+        StringBuilder sb = new StringBuilder();
+        for (char c : str) {
+            cnt[c - 'a']--;
+            while (min < 25 && cnt[min] == 0) {
+                min++;
+            }
+            stack.push(c);
+            while (!stack.isEmpty() && stack.peek() - 'a' <= min) {
+                sb.append(stack.poll());
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "zza";
+        System.out.println(robotWithString(s));
+    }
 }
--- src/LeetCodeWeeklyContest/WeeklyContset314_20221009/Main4.java
+++ src/LeetCodeWeeklyContest/WeeklyContset314_20221009/Main4.java
@@ -2,5 +2,29 @@ package LeetCodeWeeklyContest.WeeklyContset314_20221009;
 
 public class Main4 {
     //未做出
+    // 20221015 灵神题解
+    public static int numberOfPaths(int[][] grid, int k) {
+        final int MOD = (int) 1e9 + 7;
+        int m = grid.length, n = grid[0].length;
+        int[][][] dp = new int[m + 1][n + 1][k];
+        dp[1][1][grid[0][0] % k] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+                for (int v = 0; v < k; v++) {
+                    dp[i + 1][j + 1][(v + grid[i][j]) % k] =
+                            (dp[i + 1][j][v] + dp[i][j + 1][v]) % MOD;
+                }
+            }
+        }
+        return dp[m][n][0];
+    }
 
+    public static void main(String[] args) {
+        int[][] grid = {{5,2,4}, {3,0,5}, {0,7,2}};
+        int k = 3;
+        System.out.println(numberOfPaths(grid, k));
+    }
 }
