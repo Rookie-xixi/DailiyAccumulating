8fa50924db490cea4dc12e9a78767fc859c6d5ce
Rookie-xixi <63187798+Rookie-xixi@users.noreply.github.com>
2022-10-16 20:02:45 +0800

    Accumulating

---
+++ src/LeetCodeBase/ReverseInteger.java
@@ -0,0 +1,15 @@
+package LeetCodeBase;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            if (res < Integer.MIN_VALUE / 10 || res > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return res;
+    }
+}
--- src/Test/Main.java
+++ src/Test/Main.java
@@ -7,15 +7,7 @@ import static java.lang.Math.*;
 public class Main {
 
     public static void main(String[] args) {
-        // 字符串对特殊字符分割的时候需要转义
-//        String s = "a.b.c";
-//        String[] sub = s.split("\\.");
-//        for (int i = 0; i < sub.length; i++) {
-//            System.out.println(sub[i]);
-//        }
-        var generator = new Random();
-        System.out.println(generator.nextInt(1000));
-        System.out.println((int) (Math.random() * 1000));
-        System.out.println(sqrt(pow(4,2)));
+
     }
+
 }
